1dfea82d7c8f4d3f298491044b4a0bfde3cd2518
Boaardz56 <jackie.lopez.56@gmail.com>
2020-06-24 10:01:19 -0400

    adding ajax call

--- scripts.js
+++ scripts.js
@@ -10,13 +10,19 @@ var uv = $("#uvIndex");
 
 var apiKey = "472a07ec988fe068670c8e23411bee88";
 
-//setting api call
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-console.log(queryURL);
+
 //when button clicked, city name typed by user is read
 $("#submitBtn").on("click", function () {
-    console.log("element clicked");
+    console.log("element clicked");     
+    var city = inputName.val();  
+    getWeather(city);
     
+});
+
+
+function getWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(queryURL);
     //ajax call   
     $.ajax({
         url: queryURL,
@@ -24,8 +30,20 @@ $("#submitBtn").on("click", function () {
         dataType: "json"
     }).then(function(response) {
         console.log(response);
-
     });
-        
-   
-});
+
+}
+
+function getWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(queryURL);
+    //ajax call   
+    $.ajax({
+        url: queryURL,
+        type: "GET",
+        dataType: "json"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+}
