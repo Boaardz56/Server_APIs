7e1af36a0b98184786fe0a527d782b3241efe323
Boaardz56 <jackie.lopez.56@gmail.com>
2020-06-24 22:08:26 -0400

    adding in 5 day forecast information

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function() { 
 //setting variables
 var inputName = $(".cityInput");
-var submitBtn = $("#submitBtn");
 var name = $("#cityName");
-var city = $("#current-pic");
 var temp = $("#temp");
 var humidity = $("#humidity");
 var windSpeed = $("#windSpeed");
@@ -14,17 +12,17 @@ var apiKey = "472a07ec988fe068670c8e23411bee88";
 
 //when button clicked, city name typed by user is read
 $("#submitBtn").on("click", function () {
-    console.log("element clicked");     
+    // console.log("element clicked");     
     var city = inputName.val();  
     getWeather(city);
-    // getForecast(city);
+    getForecast(city);
     
 });
 
 //adding weather call in
 function getWeather(city) {
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    console.log(queryURL);
+    // console.log(queryURL);
     //ajax call   
     $.ajax({
         url: queryURL,
@@ -32,12 +30,12 @@ function getWeather(city) {
         dataType: "json"
     }).then(function(response) {
         console.log(response);
-        console.log('name', name);
+        // console.log('name', name);
             //attach city name to heading
             name.text(response.name);
             //attach date to heading
-            var currentDate = new Date();
-            console.log(currentDate);
+            // var currentDate = new Date();
+            // console.log(currentDate);
             const d = new Date('2010-08-05')
             const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
             const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
@@ -53,43 +51,57 @@ function getWeather(city) {
             humidity.text(response.main.humidity);
             //add windspeed input
             windSpeed.text(response.wind.speed);
-            //add Uv index
+        //add Uv index
             var lat=response.coord.lat;
             var lon=response.coord.lon;
-            var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat +"&lon=" + lon;
-            //ajax call
+            var UVqueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + apiKey+ "&lat=" + lat + "&lon=" + lon;
+            // console.log(UVqueryURL);
+            //ajax call for uv index
                 $.ajax({
-                    url: queryURL,
+                    url: UVqueryURL,
                     type: "GET",
                     dataType: "json"
-                }).then(function(responseUV) {
-                    var uvIndex = $("<span>");
-                    uvIndex.attr("class", "badge badge-danger");
-                    uvIndex.text = response.data[0].value;
-                    uv.append(uvIndex);
-                })
+                }).then(function(response) {
+                    // console.log(response);
+                    uv.text(response[0].value);
+                    uv.append();
+                });
+        //get 5 day forcast
+            
 
     });
-
 }
+
+function getForecast(city) {
+    var forecastqueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+   //ajax call   
+   $.ajax({
+       url: forecastqueryURL,
+       type: "GET",
+       dataType: "json"
+   }).then(function(response) {
+       var forecastData = $(".forecast");
+       //create for loop
+           for (i=0; i<forecastData.length; i++) {
+               forecastData[i].text = "";
+               var forecastIndex = i*8 + 4;
+               console.log(forecastIndex);
+               
+
+
+           }
+       
+
+
+   });
+}
+
 // change temp from K to F function
 function tempChange(K) {
     return Math.floor((K - 273.15) *1.8 +32);
 }
 // //adding forecast in
-// function getForecast(city) {
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
-//     console.log(queryURL);
-//     //ajax call   
-//     $.ajax({
-//         url: queryURL,
-//         type: "GET",
-//         dataType: "json"
-//     }).then(function(response) {
-//         console.log(response);
-//     });
 
-// }
 
 
 
